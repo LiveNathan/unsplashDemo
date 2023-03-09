283174773de82b8b7040e541bf0a59269c260e2c
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-03-09 11:47:16 -0600

    second commit

---
+++ src/main/java/cnLabs/unsplashDemo/UnsplashDemoApplication.java
@@ -0,0 +1,13 @@
+package cnLabs.unsplashDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsplashDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsplashDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cnLabs/unsplashDemo/UnsplashDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cnLabs.unsplashDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsplashDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
